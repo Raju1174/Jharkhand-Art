d3254f39eeff7d6616ca69889d185bdd966699c1
raju1174 <raju.pandit1174@gmail.com>
2026-02-02 10:00:00 +0530

    Add backend folder structure and example model

---
+++ server/src/models/Example.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const exampleSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+}, { timestamps: true });
+
+export default mongoose.model('Example', exampleSchema);
